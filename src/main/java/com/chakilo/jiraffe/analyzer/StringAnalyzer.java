dbde31aff8e7e6e83172d9d472d7afe163d678b2
Chakilo <chakilo@msn.com>
2019-02-26 16:21:11 +0800

    empty line

--- src/main/java/com/chakilo/jiraffe/analyzer/StringAnalyzer.java
+++ src/main/java/com/chakilo/jiraffe/analyzer/StringAnalyzer.java
@@ -13,6 +13,7 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
+
 package com.chakilo.jiraffe.analyzer;
 
 import com.chakilo.jiraffe.model.JSONElement;
