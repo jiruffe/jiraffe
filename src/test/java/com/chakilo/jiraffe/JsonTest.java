8eb4f94a7a5d1b172318c95409de413c71729dc7
Chakilo <chakilo@msn.com>
2019-02-28 16:11:09 +0800

    added test

--- src/test/java/com/chakilo/jiraffe/JsonTest.java
+++ src/test/java/com/chakilo/jiraffe/JsonTest.java
@@ -37,6 +37,25 @@ public class JsonTest {
     }
 
     @Test
+    public void test00() throws Exception {
+
+        String s = "{\"code\":1,\"msg\":\"Succeed\",\"data\":{\"list\":[{\"name\":\"北京\",\"id\":1},{\"name\":\"新疆\",\"id\":31},{\"name\":\"重庆\",\"id\":4},{\"name\":\"广东\",\"id\":19},{\"name\":\"浙江\",\"id\":15},{\"name\":\"天津\",\"id\":3},{\"name\":\"港澳\",\"id\":52993},{\"name\":\"广西\",\"id\":20},{\"name\":\"内蒙古\",\"id\":11},{\"name\":\"宁夏\",\"id\":30},{\"name\":\"江西\",\"id\":21},{\"name\":\"台湾\",\"id\":32},{\"name\":\"安徽\",\"id\":14},{\"name\":\"贵州\",\"id\":24},{\"name\":\"陕西\",\"id\":27},{\"name\":\"辽宁\",\"id\":8},{\"name\":\"山西\",\"id\":6},{\"name\":\"青海\",\"id\":29},{\"name\":\"四川\",\"id\":22},{\"name\":\"江苏\",\"id\":12},{\"name\":\"河北\",\"id\":5},{\"name\":\"西藏\",\"id\":26},{\"name\":\"钓鱼岛\",\"id\":84},{\"name\":\"福建\",\"id\":16},{\"name\":\"吉林\",\"id\":9},{\"name\":\"湖北\",\"id\":17},{\"name\":\"云南\",\"id\":25},{\"name\":\"海南\",\"id\":23},{\"name\":\"上海\",\"id\":2},{\"name\":\"甘肃\",\"id\":28},{\"name\":\"湖南\",\"id\":18},{\"name\":\"山东\",\"id\":13},{\"name\":\"河南\",\"id\":7},{\"name\":\"黑龙江\",\"id\":10}]}}";
+
+        JSONElement element = JSON.deserialize(s);
+
+        assert s.equals(StringUtil.unescape(element.toString()));
+
+//        long start = System.currentTimeMillis();
+//
+//        for (int i = 0; i < 100000; i++) {
+//            element = JSON.deserialize(s);
+//        }
+//
+//        System.out.println(System.currentTimeMillis() - start);
+
+    }
+
+    @Test
     public void test1() throws Exception {
 
         JSONElement element = JSON.deserialize("{'a':'b','c':{'d':'e'},'f':{'g':\"h\"},'i':[{'j':'k','l':'m'},'n']}");
